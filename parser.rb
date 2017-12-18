328ab418c09d16d4939b63bab52c14295c5e36b7
Nikolai KLimenuk <nick.klimenuk@gmail.com>
2017-12-18 12:20:24 +0200

    Added custom error class to parser.rb

--- parser.rb
+++ parser.rb
@@ -2,6 +2,10 @@ require 'net/http'
 require 'json'
 require 'singleton'
 
+class DestinationParserError < StandardError 
+
+end
+
 class GoogleApiUrl
      include Singleton
      attr_reader :url, :json, :key
@@ -22,27 +26,27 @@ end
 
 
 class DestinationParser
- 
+  
     def distance_between_cities(origin, destination)
         google_api_url = GoogleApiUrl.instance
         begin
-            raise TypeError, 'Origin must be filled' unless origin.is_a? String
-            raise TypeError, 'Destination to must be filled' unless destination.is_a? String
+            raise DestinationParserError, 'Origin must be filled' unless origin.is_a? String
+            raise DestinationParserError, 'Destination to must be filled' unless destination.is_a? String
             google_api_url.origins = origin
             google_api_url.destinations = destination
             uri = URI(google_api_url.url + google_api_url.json + '&origins=' + google_api_url.origins +
                        '&destinations=' + google_api_url.destinations + '&key' +
                                           google_api_url.key )                          
             parsed_data = JSON.parse(Net::HTTP.get(uri))  
-            raise NoMethodError, 'Please provide correct origin' if parsed_data['origin_addresses'][0].empty?
-            raise NoMethodError, 'Please provide correct destination' if parsed_data['destination_addresses'][0].empty?
+            raise DestinationParserError, 'Please provide correct origin' if parsed_data['origin_addresses'][0].empty?
+            raise DestinationParserError, 'Please provide correct destination' if parsed_data['destination_addresses'][0].empty?
             distance = parsed_data['rows'][0]['elements'][0]['distance']['text'] 
             time = parsed_data['rows'][0]['elements'][0]['duration']['text']
             return distance + ', ' + time
 
-            rescue TypeError => e
+            rescue DestinationParserError => e
                 p e.message  
-            rescue NoMethodError => e 
+            rescue DestinationParserError => e 
                 p e.message
         end
     end
